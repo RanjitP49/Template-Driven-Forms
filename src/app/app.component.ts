ede3114ed9085c470faa4d1982acda25abbc7dcd
RanjitP49 <patil.ranjit49@gmail.com>
2022-04-22 19:02:42 +0530

    form added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
+  @ViewChild('f') signUpForm: NgForm;
   name = 'Angular ' + VERSION.major;
+  Games: string[] = ['Cricket', 'Chess', 'Football'];
+
+  onSubmit(f: NgForm) {
+    console.log(f.value);
+    this.signUpForm.reset();
+  }
 }
